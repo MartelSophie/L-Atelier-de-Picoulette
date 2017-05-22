5b99d9fd8894f287ee2857c144886feef6cf3ac6
MARTELSophie <sophie.martel@hotmail.fr>
2017-05-22 12:00:46 +0200

    ajout ancres, target blank, tentative js menu statique

---
+++ app.js
@@ -0,0 +1,11 @@
+var  mn = $(".main_navbar");
+    mns = "main_navbar_scrolled";
+    hdr = $('header').height();
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdr ) {
+    mn.addClass(mns);
+  } else {
+    mn.removeClass(mns);
+  }
+});
